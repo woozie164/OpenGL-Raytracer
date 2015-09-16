c31f6bf0a5fe79d42f7646774c31958ba8ebd880
woozie164 <woozie663@gmail.com>
2015-09-16 23:42:47 +0200

    Added glfw error callback

--- OpenGL Raytracer/main.cpp
+++ OpenGL Raytracer/main.cpp
@@ -1,6 +1,18 @@
 #include "glfw3.h"
+#include <iostream>
+
+void glfw_error_callback(int error, const char* description)
+{
+	std::cerr << description << std::endl;
+}
 
 void main() {
+	glfwSetErrorCallback(glfw_error_callback);
 	glfwInit();
-	glfwCreateWindow(800, 800, "OpenGL Raytracer", 0, 0);
+	GLFWwindow * window = glfwCreateWindow(800, 800, "OpenGL Raytracer", 0, 0);
+	glfwMakeContextCurrent(window);
+	while (!glfwWindowShouldClose(window))
+	{
+		// Keep running
+	}
 }
