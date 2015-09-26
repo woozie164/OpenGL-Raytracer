513e18a9b574dbf741380f030609f9f0313c76e8
woozie164 <woozie663@gmail.com>
2015-09-26 14:58:26 +0200

    Camera is less buggy now
    Works better now when you move closer or further away from the triangle.
    Still really buggy when viewing the triangle from the side, though.

--- OpenGL Raytracer/raytracer_cs.glsl
+++ OpenGL Raytracer/raytracer_cs.glsl
@@ -70,7 +70,7 @@ void main(void)
 	
 	float t;
 	vec4 color;
-	vec3 ray_dir = s;
+	vec3 ray_dir = camera_pos + (s - camera_pos) * NEAR_PLANE_DIST;
 	//vec3 intersectionPoint = camera_pos = camera_dir * t;
 	if(RayVsTriangle(camera_pos, ray_dir, x, y, z, t)) {
 	//if(RayVsTriangle(vec3(0.25, -0.25, 0), vec3(0.0, 0.0, 1.0), x, y, z, t)) {
