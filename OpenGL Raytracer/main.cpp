8a8fc457363f3b19c74faec928462c0604b67af5
woozie164 <woozie663@gmail.com>
2015-11-10 21:34:54 +0100

    Fixed file loading error

--- OpenGL Raytracer/main.cpp
+++ OpenGL Raytracer/main.cpp
@@ -177,12 +177,14 @@ int main() {
 
 	GLuint tex_2d = SOIL_load_OGL_texture
 		(
-		"img.png",
+		"img_test.png",
 		SOIL_LOAD_AUTO,
 		SOIL_CREATE_NEW_ID,
 		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
 		);
-
+	if (tex_2d == 0) {
+		printf("SOIL loading error: '%s'\n", SOIL_last_result());
+	}
 	Camera camera;
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 
