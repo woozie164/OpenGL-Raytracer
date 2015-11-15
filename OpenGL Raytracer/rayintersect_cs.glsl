df71ef906b4a2dfb0c30359d39678c6cce2a2e0d
woozie164 <woozie663@gmail.com>
2015-11-15 17:03:42 +0100

    Changed the ray's origin point

--- OpenGL Raytracer/rayintersect_cs.glsl
+++ OpenGL Raytracer/rayintersect_cs.glsl
@@ -1,7 +1,12 @@
 void main(void) 
 {
 	ivec2 storePos = ivec2(gl_GlobalInvocationID.xy);
-	ray my_ray = rays[storePos.x + storePos.y * 800];	
+	ray my_ray = rays[storePos.x + storePos.y * 800];
+	
+	// Move the ray's origin point slighly forward so that 
+	// it avoids interesections with the primitive that spawned it.
+	my_ray.origin = my_ray.origin + my_ray.dir * 0.001;
+		
 	trace(my_ray);
 	rays[storePos.x + storePos.y * 800] = my_ray;
 }
