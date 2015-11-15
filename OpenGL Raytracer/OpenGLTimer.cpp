acb92ff800427ed61792bd7b773ffaa7f2dfd3a3
woozie164 <woozie663@gmail.com>
2015-11-15 21:18:57 +0100

    Added an opengl timer

---
+++ OpenGL Raytracer/OpenGLTimer.cpp
@@ -0,0 +1,39 @@
+#include "OpenGLTimer.h"
+
+
+OpenGLTimer::OpenGLTimer()
+{
+	glGenQueries(1, &startQuery);
+	glGenQueries(1, &endQuery);
+}
+
+
+OpenGLTimer::~OpenGLTimer()
+{
+}
+
+
+void OpenGLTimer::Start()
+{
+	glQueryCounter(startQuery, GL_TIMESTAMP);
+}
+
+
+void OpenGLTimer::End()
+{
+	glQueryCounter(endQuery, GL_TIMESTAMP);
+}
+
+// Return time in milliseconds
+GLuint64 OpenGLTimer::GetElapsedTime()
+{
+	GLint stopTimerAvailable = 0;
+	while (!stopTimerAvailable)
+	{
+		glGetQueryObjectiv(endQuery, GL_QUERY_RESULT_AVAILABLE, &stopTimerAvailable);
+	}
+	glGetQueryObjectui64v(startQuery, GL_QUERY_RESULT, &startTime);
+	glGetQueryObjectui64v(endQuery, GL_QUERY_RESULT, &endTime);
+	elapsedTime = (endTime - startTime) / 1000000.0;
+	return elapsedTime;
+}
---
+++ OpenGL Raytracer/OpenGLTimer.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "glew-1.13.0\include\GL\glew.h"
+
+class OpenGLTimer
+{
+	GLuint startQuery, endQuery;
+	GLuint64 startTime, endTime;
+	GLuint64 elapsedTime;
+public:
+	OpenGLTimer();
+	~OpenGLTimer();
+
+	void Start();
+	void End();
+	GLuint64 GetElapsedTime();
+};
+
