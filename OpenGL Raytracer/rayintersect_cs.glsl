c3f843d653569b668614b5cf303b738e82d5e95b
woozie164 <woozie663@gmail.com>
2016-03-29 06:03:25 +0200

    Moved depth texture write
    Still something wrong though. Doesn't look like it writes depth values.

--- OpenGL Raytracer/rayintersect_cs.glsl
+++ OpenGL Raytracer/rayintersect_cs.glsl
@@ -4,14 +4,15 @@ void main(void)
 	if(storePos.x > windowWidth) return;
 	int i = storePos.x + storePos.y * windowHeight;
 	ray my_ray = rays[i];
-	
-	float d = distance(my_ray.origin, camera_pos);
-	imageStore(depthTexture, storePos, vec4(1.0)/*vec4(d)*/);
-	
+		
 	// Move the ray's origin point slighly forward so that 
 	// it avoids interesections with the primitive that spawned it.
 	my_ray.origin = my_ray.origin + my_ray.dir * 0.001;
 		
 	trace(my_ray);
+	
+	float d = distance(my_ray.origin, camera_pos);
+	imageStore(depthTexture, storePos, vec4(d));
+	
 	rays[i] = my_ray;
 }
